5823c2d0219c5eb2fd0a8f40a521d3fdcd42e296
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-04-22 16:58:34 +0900

    ミニマム構成まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, null: false
+
+
+  def email_required?
+    false
+  end
+
 end
